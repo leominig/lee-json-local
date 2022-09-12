a4e89dec01742d734801b5dea74f4c31844aca04
leominig <leonardominig@gmail.com>
2022-09-12 13:56:43 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Datos />
+</template>
+
+<script setup>
+import Datos from './components/Datos.vue'
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Datos.vue
@@ -0,0 +1,45 @@
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="table-responsive">
+          <table class="table">
+            <thead class="table-dark">
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Precio</th>
+                <th scope="col">Stock</th>
+                <th scope="col">Imagen</th>
+              </tr>
+            </thead>
+            <tbody class="table table-dark table-striped">
+              <tr v-for="item in items" :key="item.id">
+                <th scope="row">{{ item.id }}</th>
+                <td>{{ item.nombre }}</td>
+                <td>{{ item.precio }}</td>
+                <td>{{ item.stock }}</td>
+                <td>{{ item.img }} {{ item.nombre }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import { ref } from 'vue'
+import datos from '../assets/postres.json'
+import { computed } from 'vue'
+
+defineProps: ['msg']
+
+const items = ref(datos)
+
+
+
+</script>  
